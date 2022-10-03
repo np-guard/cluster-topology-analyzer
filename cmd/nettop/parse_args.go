58f045d8f2a870befd69e52e8f4f051f7b3dca11
Ziv Nevo <nevo@il.ibm.com>
2022-10-03 10:06:48 +0300

    nicer help for "format" switch
    Signed-off-by: Ziv Nevo <nevo@il.ibm.com>

--- cmd/nettop/parse_args.go
+++ cmd/nettop/parse_args.go
@@ -22,7 +22,7 @@ type InArgs struct {
 func ParseInArgs(args *InArgs) error {
 	args.DirPath = flag.String("dirpath", "", "input directory path")
 	args.OutputFile = flag.String("outputfile", "", "file path to store results")
-	args.OutputFormat = flag.String("format", JSONFormat, "output format (must be either json or yaml)")
+	args.OutputFormat = flag.String("format", JSONFormat, "output format; must be either \"json\" or \"yaml\"")
 	args.SynthNetpols = flag.Bool("netpols", false, "whether to synthesize NetworkPolicies to allow only the discovered connections")
 	args.Quiet = flag.Bool("q", false, "runs quietly, reports only severe errors and results")
 	args.Verbose = flag.Bool("v", false, "runs with more informative messages printed to log")
