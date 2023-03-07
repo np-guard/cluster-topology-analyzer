358e2ce9fe3bb172e4ed5e99693f29466dbdb789
Ziv Nevo <nevo@il.ibm.com>
2023-03-07 16:31:51 +0200

    properly build fully qualified resource names
    Signed-off-by: Ziv Nevo <nevo@il.ibm.com>

--- pkg/controller/utils.go
+++ pkg/controller/utils.go
@@ -1,5 +1,9 @@
 package controller
 
+import (
+	"k8s.io/apimachinery/pkg/types"
+)
+
 func stopProcessing(stopOn1stErr bool, errs []FileProcessingError) bool {
 	for idx := range errs {
 		if errs[idx].IsFatal() || stopOn1stErr && errs[idx].IsSevere() {
@@ -14,4 +18,9 @@ func appendAndLogNewError(errs []FileProcessingError, newErr *FileProcessingErro
 	logError(logger, newErr)
 	errs = append(errs, *newErr)
 	return errs
+}
+
+func namespacedName(namespace, resource string) string {
+	namespacedRes := types.NamespacedName{Namespace: namespace, Name: resource}
+	return namespacedRes.String()
 }
