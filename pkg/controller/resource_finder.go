b819bd98408ffd1d50358d971e2c3f54a4b59527
Ziv Nevo <nevo@il.ibm.com>
2023-04-20 14:52:02 +0300

    fully parsing each yaml doc in one iteration
    Signed-off-by: Ziv Nevo <nevo@il.ibm.com>

--- pkg/controller/resource_finder.go
+++ pkg/controller/resource_finder.go
@@ -32,27 +32,21 @@ const (
 var (
 	acceptedK8sTypesRegex = fmt.Sprintf("(^%s$|^%s$|^%s$|^%s$|^%s$|^%s$|^%s$|^%s$|^%s$|^%s$)",
 		pod, replicaSet, replicationController, deployment, daemonSet, statefulSet, job, cronJob, service, configmap)
-	acceptedK8sTypes = regexp.MustCompile(acceptedK8sTypesRegex)
-	yamlSuffix       = regexp.MustCompile(".ya?ml$")
-	decoder          = scheme.Codecs.UniversalDeserializer()
+	acceptedK8sTypes   = regexp.MustCompile(acceptedK8sTypesRegex)
+	yamlSuffix         = regexp.MustCompile(".ya?ml$")
+	k8sResourceDecoder = scheme.Codecs.UniversalDeserializer()
 )
 
-// rawK8sResource stores a raw K8s resource and its kind for later parsing
-type rawK8sResource struct {
-	GroupKind     string
-	RuntimeObject []byte
-}
-
-// resourceFinder is used to locate all relevant K8s resources in a given file-system directory
+// resourceFinder is used to locate all relevant K8s resources in given file-system directories
 // and to convert them into the internal structs, used for later processing.
 type resourceFinder struct {
 	logger       Logger
 	stopOn1stErr bool
 	walkFn       WalkFunction // for customizing directory scan
 
-	workloads  []common.Resource
-	services   []common.Service
-	configmaps []common.CfgMap
+	workloads  []common.Resource // accumulates all workload resources found
+	services   []common.Service  // accumulates all service resources found
+	configmaps []common.CfgMap   // accumulates all ConfigMap resources found
 }
 
 // getRelevantK8sResources is the main function of resourceFinder.
@@ -70,13 +64,8 @@ func (rf *resourceFinder) getRelevantK8sResources(repoDir string) []FileProcessi
 	}
 
 	for _, mfp := range manifestFiles {
-		rawK8sResources, err := rf.parseK8sYaml(mfp)
-		fileScanErrors = append(fileScanErrors, err...)
-		if stopProcessing(rf.stopOn1stErr, fileScanErrors) {
-			return fileScanErrors
-		}
-		manifestFilePath := pathWithoutBaseDir(mfp, repoDir)
-		errs := rf.parseResources(rawK8sResources, manifestFilePath)
+		relMfp := pathWithoutBaseDir(mfp, repoDir)
+		errs := rf.parseK8sYaml(mfp, relMfp)
 		fileScanErrors = append(fileScanErrors, errs...)
 		if stopProcessing(rf.stopOn1stErr, fileScanErrors) {
 			return fileScanErrors
@@ -139,63 +128,60 @@ func (rf *resourceFinder) splitByYamlDocuments(mfp string) ([][]byte, []FileProc
 	return documents, nil
 }
 
-// parseK8sYaml takes a YAML document and checks if it stands for a relevant K8s resource.
-// If yes, it puts it into a rawK8sResource and appends it to the result.
-func (rf *resourceFinder) parseK8sYaml(mfp string) ([]rawK8sResource, []FileProcessingError) {
-	dObjs := []rawK8sResource{}
+// parseK8sYaml takes a YAML file and attempts to parse each of its documents into
+// one of the relevant k8s resources
+func (rf *resourceFinder) parseK8sYaml(mfp, relMfp string) []FileProcessingError {
 	yamlDocs, fileProcessingErrors := rf.splitByYamlDocuments(mfp)
 	if stopProcessing(rf.stopOn1stErr, fileProcessingErrors) {
-		return nil, fileProcessingErrors
+		return fileProcessingErrors
 	}
 
 	for docID, doc := range yamlDocs {
-		_, groupVersionKind, err := decoder.Decode(doc, nil, nil)
+		_, groupVersionKind, err := k8sResourceDecoder.Decode(doc, nil, nil)
 		if err != nil {
-			fileProcessingErrors = appendAndLogNewError(fileProcessingErrors, notK8sResource(mfp, docID, err), rf.logger)
+			fileProcessingErrors = appendAndLogNewError(fileProcessingErrors, notK8sResource(relMfp, docID, err), rf.logger)
 			continue
 		}
 		if !acceptedK8sTypes.MatchString(groupVersionKind.Kind) {
-			rf.logger.Infof("in file: %s, document: %d, skipping object with type: %s", mfp, docID, groupVersionKind.Kind)
+			rf.logger.Infof("in file: %s, document: %d, skipping object with type: %s", relMfp, docID, groupVersionKind.Kind)
 		} else {
-			dObjs = append(dObjs, rawK8sResource{GroupKind: groupVersionKind.Kind, RuntimeObject: doc})
+			kind := groupVersionKind.Kind
+			err = rf.parseResource(kind, doc, relMfp)
+			if err != nil {
+				fileProcessingErrors = appendAndLogNewError(fileProcessingErrors, failedScanningResource(kind, relMfp, err), rf.logger)
+			}
 		}
 	}
-	return dObjs, fileProcessingErrors
+	return fileProcessingErrors
 }
 
-// parseResources takes raw K8s resources in a given yaml file and puts each parsed resource into one of the 3 struct slices:
+// parseResource takes a yaml document, parses it into a K8s resource and puts it into one of the 3 struct slices:
 // the workload resource slice, the Service resource slice, and the ConfigMaps resource slice
-func (rf *resourceFinder) parseResources(rawK8sResources []rawK8sResource, manifestFilePath string) []FileProcessingError {
-	parseErrors := []FileProcessingError{}
-	for _, p := range rawK8sResources {
-		switch p.GroupKind {
-		case service:
-			res, err := analyzer.ScanK8sServiceObject(p.GroupKind, p.RuntimeObject)
-			if err != nil {
-				parseErrors = appendAndLogNewError(parseErrors, failedScanningResource(p.GroupKind, manifestFilePath, err), rf.logger)
-				continue
-			}
-			res.Resource.FilePath = manifestFilePath
-			rf.services = append(rf.services, res)
-		case configmap:
-			res, err := analyzer.ScanK8sConfigmapObject(p.GroupKind, p.RuntimeObject)
-			if err != nil {
-				parseErrors = appendAndLogNewError(parseErrors, failedScanningResource(p.GroupKind, manifestFilePath, err), rf.logger)
-				continue
-			}
-			rf.configmaps = append(rf.configmaps, res)
-		default:
-			res, err := analyzer.ScanK8sWorkloadObject(p.GroupKind, p.RuntimeObject)
-			if err != nil {
-				parseErrors = appendAndLogNewError(parseErrors, failedScanningResource(p.GroupKind, manifestFilePath, err), rf.logger)
-				continue
-			}
-			res.Resource.FilePath = manifestFilePath
-			rf.workloads = append(rf.workloads, res)
+func (rf *resourceFinder) parseResource(kind string, yamlDoc []byte, manifestFilePath string) error {
+	switch kind {
+	case service:
+		res, err := analyzer.ScanK8sServiceObject(kind, yamlDoc)
+		if err != nil {
+			return err
 		}
+		res.Resource.FilePath = manifestFilePath
+		rf.services = append(rf.services, res)
+	case configmap:
+		res, err := analyzer.ScanK8sConfigmapObject(kind, yamlDoc)
+		if err != nil {
+			return err
+		}
+		rf.configmaps = append(rf.configmaps, res)
+	default:
+		res, err := analyzer.ScanK8sWorkloadObject(kind, yamlDoc)
+		if err != nil {
+			return err
+		}
+		res.Resource.FilePath = manifestFilePath
+		rf.workloads = append(rf.workloads, res)
 	}
 
-	return parseErrors
+	return nil
 }
 
 // returns a file path without its prefix base dir
